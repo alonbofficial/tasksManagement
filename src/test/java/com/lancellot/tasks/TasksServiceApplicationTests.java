a8cf66b41b123c826bcb1150abdc499062e3f7b6
Alon B <alon.bu@kidslly.com>
2025-12-25 15:06:40 +0200

    initialize tasks-service application with Spring Boot and MongoDB support

---
+++ src/test/java/com/lancellot/tasks/TasksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lancellot.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
